65085f115659dc21e643f1a288c789235347303a
Riccardo Giuntoli <riccardo@telecomlobby.com>
2021-03-11 12:08:13 +0100

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch taglio-11032021/0001
     Your branch is up to date with 'origin/taglio-11032021/0001'.
    
     Changes to be committed:
    	modified:   README.md
    	modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,9 @@
 #!/bin/ksh
 
-HOST=$1
-DOMAIN=$2
-
-if [[ $# -eq 0]];then
-  print "use $0 [host] [domain]"
-  exit
-fi
-
-echo $HOST.$DOMAIN > /etc/myname
-
-mkdir ~/Bin ~/Sources ~/Sources/Git ~/Download
-
-pkg_add nano colorls wget
-wget https://raw.githubusercontent.com/redeltaglio/OpenBSD/master/root.nanorc -O /root/.nanorc
-cat /etc/examples/doas.conf | sed s/keepenv/"persist keepenv"/ > /etc/doas.conf
-wget 
+function main {
+	echo "changing installurl"
+	echo "https://cdn.openbsd.org/pub/OpenBSD" > /etc/installurl
+	echo "adding packages"
+	pkg_add colorls nano wget
+}
+main
