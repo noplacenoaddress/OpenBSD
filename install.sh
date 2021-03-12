4bff9d05ab3226a362675cb4b7e3870ea9d71592
Riccardo Giuntoli <riccardo@telecomlobby.com>
2021-03-12 08:07:45 +0100

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch taglio-11032021/0001
     Your branch is ahead of 'master' by 5 commits.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   README.md
    	deleted:    install.sh
    	new file:   setup_node
    	modified:   src/etc/pf.conf

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/ksh
-
-function main {
-	echo "changing installurl"
-	echo "https://cdn.openbsd.org/pub/OpenBSD" > /etc/installurl
-	echo "adding packages"
-	pkg_add colorls nano wget
-}
-main
