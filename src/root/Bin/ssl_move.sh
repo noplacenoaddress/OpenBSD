a4fd762278ce935278feb7a4e255f531fdc78eeb
Riccardo Giuntoli <riccardo@telecomlobby.com>
2021-04-08 14:59:33 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch taglio-08042021
     Your branch is ahead of 'origin/taglio-08042021' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   OpenBSD.tar
    	modified:   src/etc/httpd.conf
    	modified:   src/root/Bin/ssl_move.sh

--- src/root/Bin/ssl_move.sh
+++ src/root/Bin/ssl_move.sh
@@ -6,13 +6,14 @@ cd /etc/ssl
 case $OPTION in
 	"b" )
 		mv *telecomlobby* backup/
-		mv private/*telecomlobby* backup/private/ ;;
+		mv private/*telecomlobby* backup/private/ 
+		cp /etc/httpd.conf backup/ ;;
 	"u" )
 		mv backup/*telecomlobby* .
-		mv backup/private/*telecomlobby* private/ ;;
+		mv backup/private/*telecomlobby* private/ 
+		mv backup/httpd.conf /etc ;;
 	* )
 		print "use b or u"
 		exit 1 ;;
 esac
-
 
