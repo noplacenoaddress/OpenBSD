2fc76f088f964bb5f181c6a47f7ae549d2eead53
Riccardo Giuntoli <riccardo@telecomlobby.com>
2021-04-09 11:37:42 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch taglio-09042021
     Your branch is up to date with 'origin/taglio-09042021'.
    
     Changes to be committed:
    	modified:   OpenBSD.tar
    	modified:   src/etc/httpd-nossl.conf
    	modified:   src/etc/httpd.conf
    	modified:   src/root/Bin/ssl_move.sh

--- src/root/Bin/ssl_move.sh
+++ src/root/Bin/ssl_move.sh
@@ -1,30 +1,27 @@
 #!/bin/ksh
 
 OPTION=$1
+$BACKUP="/root/Backups/ssl"
 
 cp /home/taglio/Sources/Git/OpenBSD/src/etc/acme-client.conf /etc
 cd /etc/ssl
 case $OPTION in
 	"b" )
-		mv *telecomlobby* backup/
-		mv *redama* backup/
-		mv private/*telecomlobby* backup/private/ 
-		mv private/*redama* backup/private/
-		cp /etc/httpd.conf backup/ 
+		mv *telecomlobby* $BACKUP/
+		mv private/*telecomlobby* $BACKUP/private/ 
+		cp /etc/httpd.conf $BACKUP/ 
 		cp /home/taglio/Sources/Git/OpenBSD/src/etc/httpd-nossl.conf /etc/httpd.conf
 		httpd -n 
 		rcctl restart httpd ;;
 	"u" )
-		mv backup/*telecomlobby* .
-		mv backup/*redama* .
-		mv backup/private/*telecomlobby* private/ 
-		mv backup/private/*redama* private/
-		mv backup/httpd.conf /etc 
+		mv $BACKUP/*telecomlobby* .
+		mv $BACKUP/private/*telecomlobby* private/ 
+		mv $BACKUP/httpd.conf /etc 
 		httpd -n
 		rcctl restart httpd ;;
 	"c" )
-		tar -cvf /root/Backups/ssl_backup.tar backup/
-		rm -rf backup/*
+		tar -cvf /root/$BACKUPs/ssl_backup.tar $BACKUP/
+		rm -rf $BACKUP/*
 		cp /home/taglio/Sources/Git/OpenBSD/src/etc/httpd.conf /etc/
 		httpd -n
 		rcctl restart httpd ;;
