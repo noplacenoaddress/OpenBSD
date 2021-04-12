60d5bc2e7a68192bb3e07d3cfec77000b1cd11f9
Riccardo Giuntoli <riccardo@telecomlobby.com>
2021-04-12 09:46:36 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch taglio-12042021
     Your branch is up to date with 'origin/taglio-12042021'.
    
     Changes to be committed:
    	modified:   OpenBSD.tar
    	modified:   setup_node
    	modified:   src/etc/iked.conf
    	new file:   src/etc/iked/ca/ca.crt
    	modified:   src/root/Bin/ssl_pk12_cert_pub_priv_extract.sh
    	modified:   src/var/unbound/etc/unbound.conf
    	new file:   src/var/unbound/etc/unbound_ad_servers

--- src/root/Bin/ssl_pk12_cert_pub_priv_extract.sh
+++ src/root/Bin/ssl_pk12_cert_pub_priv_extract.sh
@@ -7,7 +7,7 @@ fi
 
 
 openssl pkcs12 -in $1 -nocerts -out /etc/iked/private/local.key
-cp /etc/iked/private/local.key -out /etc/isakmpd/private/local.key
+cp /etc/iked/private/local.key /etc/isakmpd/private/local.key
 openssl pkcs12 -in $1 -clcerts -nokeys -out /etc/iked/certs/uk.telecomlobby.com.crt
 cp /etc/iked/certs/uk.telecomlobby.com.crt /etc/isakmpd/certs/
 openssl x509 -pubkey -noout -in /etc/iked/certs/uk.telecomlobby.com.crt  > /etc/iked/local.pub
