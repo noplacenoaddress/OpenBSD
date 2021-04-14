e914a5f98adeaf8545ac92c70893e0a113f7e23b
Riccardo Giuntoli <riccardo@telecomlobby.com>
2021-04-14 22:12:58 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch taglio-14042021
     Your branch is up to date with 'origin/taglio-14042021'.
    
     Changes to be committed:
    	modified:   src/root/Bin/ssl_pk12_cert_pub_priv_extract.sh

--- src/root/Bin/ssl_pk12_cert_pub_priv_extract.sh
+++ src/root/Bin/ssl_pk12_cert_pub_priv_extract.sh
@@ -8,9 +8,9 @@ fi
 
 openssl pkcs12 -in $1 -nocerts -out /etc/iked/private/local.key
 cp /etc/iked/private/local.key /etc/isakmpd/private/local.key
-openssl pkcs12 -in $1 -clcerts -nokeys -out /etc/iked/certs/uk.telecomlobby.com.crt
-cp /etc/iked/certs/uk.telecomlobby.com.crt /etc/isakmpd/certs/
-openssl x509 -pubkey -noout -in /etc/iked/certs/uk.telecomlobby.com.crt  > /etc/iked/local.pub
+openssl pkcs12 -in $1 -clcerts -nokeys -out /etc/iked/certs/$1.crt
+cp /etc/iked/certs/$1.crt /etc/isakmpd/certs/
+openssl x509 -pubkey -noout -in /etc/iked/certs/$1.crt  > /etc/iked/local.pub
 cp /etc/iked/local.pub /etc/isakpmd/local.pub
 
 
