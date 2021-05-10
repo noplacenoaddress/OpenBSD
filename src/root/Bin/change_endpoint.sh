d9ddbb2c86604fd490966e58bd65fe402576b310
Riccardo Giuntoli <riccardo@telecomlobby.com>
2021-05-10 16:40:09 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch taglio-10052021
     Your branch is up to date with 'origin/taglio-10052021'.
    
     Changes to be committed:
    	modified:   src/etc/iked.conf.edgeos
    	modified:   src/root/Bin/change_endpoint.sh

--- src/root/Bin/change_endpoint.sh
+++ src/root/Bin/change_endpoint.sh
@@ -4,7 +4,7 @@ NEWIP=$(dig +short @8.8.8.8 cat-01.hopto.org)
 OLDIP=$(ifconfig $1 | grep tunnel | cut -d ' ' -f5)
 
 echo "updating PF"
-sed -i 's/$OLDIP/$NEWIP/g' /etc/pf.conf
+sed -i 's/$OLDIP/$NEWIP/g' /etc/{pf.conf,pf.conf.*}
 pfctl -f /etc/pf.conf
 echo "updating IKED"
 sed -i 's/$OLDIP/$NEWIP/g' /etc/iked.conf
@@ -13,3 +13,4 @@ echo "updating GRE"
 sed -i 's/$OLDIP/$NEWIP/g' /etc/hostname.$1
 ifconfig $1 destroy
 sh /etc/netstart $1
+
