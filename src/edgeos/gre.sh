9772bf7279c4a22f4b4929e11b5126c218f9f2a0
Riccardo Giuntoli <riccardo@telecomlobby.com>
2021-06-28 16:07:17 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch taglio-28062021
     Your branch is up to date with 'origin/taglio-28062021'.
    
     Changes to be committed:
    	modified:   console
    	modified:   src/edgeos/gre.sh
    	modified:   src/edgeos/ospf.sh

--- src/edgeos/gre.sh
+++ src/edgeos/gre.sh
@@ -1,15 +1,18 @@
-configure
-set interfaces tunnel /TUN/
-set interfaces tunnel /TUN/ address /GREPOPIP//30
-set interfaces tunnel /TUN/ description /PUBLICHOST/
-set interfaces tunnel /TUN/ encapsulation gre
-set interfaces tunnel /TUN/ firewall
-set interfaces tunnel /TUN/ firewall local
-set interfaces tunnel /TUN/ firewall local name GRE
-set interfaces tunnel /TUN/ local-ip 0.0.0.0
-set interfaces tunnel /TUN/ mtu 1392
-set interfaces tunnel /TUN/ multicast enable
-set interfaces tunnel /TUN/ remote-ip /PUBLICIP/
-set interfaces tunnel /TUN/ ttl 255
-commit
-save
+#!/bin/vbash
+
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper begin
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ address /GREPOPIP//30
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ description /PUBLICHOST/
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ encapsulation gre
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ firewall
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ firewall local
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ firewall local name GRE
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ local-ip 0.0.0.0
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ mtu 1392
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ multicast enable
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ remote-ip /PUBLICIP/
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ ttl 255
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper commit
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper save
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper endexit
--- src/edgeos/ospf.sh
+++ src/edgeos/ospf.sh
@@ -1,12 +1,16 @@
-configure
-set interfaces tunnel /TUN/ ip ospf authentication md5
-set interfaces tunnel /TUN/ ip ospf authentication md5 key-id 1 md5-key /OSPFMD5/
-set interfaces tunnel /TUN/ ip ospf cost /METRIC/
-set interfaces tunnel /TUN/ dead-interval 40
-set interfaces tunnel /TUN/ hello-interval 10
-set interfaces tunnel /TUN/ network point-to-point
-set interfaces tunnel /TUN/ priority 1 
-set interfaces tunnel /TUN/ retransmit-interval 5
-set interfaces tunnel /TUN/ transmit-delay 1
-commit
-save
+#!/bin/vbash
+
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper begin
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ ip ospf authentication md5
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ ip ospf authentication md5 key-id 1 md5-key /OSPFMD5/
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ ip ospf cost /METRIC/
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ dead-interval 40
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ hello-interval 10
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ network point-to-point
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ priority 1
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ retransmit-interval 5
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ transmit-delay 1
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper commit
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper save
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper endexit
+
