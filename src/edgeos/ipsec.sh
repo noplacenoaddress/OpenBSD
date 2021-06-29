f90f15d5a1218d9db5ffe4c897924dfe7177e1f1
Riccardo Giuntoli <riccardo@telecomlobby.com>
2021-06-29 08:19:34 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch taglio-29062021
     Your branch is up to date with 'origin/taglio-29062021'.
    
     Changes to be committed:
    	modified:   console
    	modified:   src/edgeos/ipsec.sh

--- src/edgeos/ipsec.sh
+++ src/edgeos/ipsec.sh
@@ -2,3 +2,5 @@
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set system task-scheduler task ES/PUBLICHOSTNAME/
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set system task-scheduler task ES/PUBLICHOSTNAME/ executable path /config/scripts/ES/PUBLICHOSTNAME/_netwatch.sh
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set system task-scheduler task ES/PUBLICHOSTNAME/ interval 1m
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper commit
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper save
