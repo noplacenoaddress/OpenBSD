94b115754f127274024358170164739832e6c5dd
Riccardo Giuntoli <riccardo@telecomlobby.com>
2021-07-18 06:00:09 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch taglio-18072021
     Your branch is up to date with 'origin/taglio-18072021'.
    
     Changes to be committed:
    	modified:   console
    	modified:   src/edgeos/ospf.sh
    	modified:   src/mikrotik/ospfd.rsc
    	modified:   src/openbsd/ospfd.conf.openbsd

--- src/edgeos/ospf.sh
+++ src/edgeos/ospf.sh
@@ -1,7 +1,6 @@
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper begin
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ ip ospf authentication md5
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ ip ospf authentication md5 key-id 1 md5-key /OSPFMD5/
-/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ ip ospf cost /METRIC/
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ ip ospf dead-interval 40
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ ip ospf hello-interval 10
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ ip ospf network point-to-point
--- src/mikrotik/ospfd.rsc
+++ src/mikrotik/ospfd.rsc
@@ -1,5 +1,5 @@
 /routing ospf interface
-add authentication=md5 authentication-key=/OSPFMD5/ comment=/POPHOSTNAME/-/PUBLICHOSTNAME/ cost=/METRIC/ interface=[/interface get [/interface gre find where comment="/HOSTNAME/"] name] network-type=point-to-point
+add authentication=md5 authentication-key=/OSPFMD5/ comment=/POPHOSTNAME/-/PUBLICHOSTNAME/ interface=[/interface get [/interface gre find where comment="/HOSTNAME/"] name] network-type=point-to-point
 /routing ospf network
 add area=backbone network=/GRENETWORK//30
 
