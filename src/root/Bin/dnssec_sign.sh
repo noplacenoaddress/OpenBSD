1fb22fa39bd92a5e1f6ab467cfef2a8ab7ca62f8
Riccardo Giuntoli <riccardo@telecomlobby.com>
2021-07-29 11:43:37 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch taglio-29072021
     Your branch is up to date with 'origin/taglio-29072021'.
    
     Changes to be committed:
    	modified:   src/root/Bin/dnssec_sign.sh

--- src/root/Bin/dnssec_sign.sh
+++ src/root/Bin/dnssec_sign.sh
@@ -1,8 +1,5 @@
 #!/bin/ksh
 
-#https://dnsviz.net/d/dreamhost.com/dnssec/
-#https://help.dreamhost.com/hc/es/articles/219539467-Generalidades-DNSSEC
-
 DOMAIN=$1
 ZONEDIR="/var/nsd/zones/master"
 DNSSECDIR="/var/nsd/etc/dnssec"
@@ -15,21 +12,20 @@ DNSSECDIR="/var/nsd/etc/dnssec"
 
 [[ $2 == "clean" ]] && \
 	rm $DNSSECDIR/*$DOMAIN* && \
-	
 	rm $ZONEDIR/$DOMAIN.zone.signed && \
 	exit
 	
 [[ $1 == "cleanall" ]] && \
 	rm -rf $DNSSECDIR/* && \
-	rm $ZONEDIR/*.signed && \
+	rm -rf $ZONEDIR/*.signed && \
 	exit
 
 [[ $2 == "reload" ]] && \
 	ZSK=$(basename $(grep -r "`grep '(zsk)' *.signed |cut -f3-10`" $DNSSECDIR/K$DOMAIN.*.key | cut -d ':' -f1) .key) && \
 	KSK=$(basename $(grep -r "`grep '(ksk)' *.signed |cut -f3-10`" $DNSSECDIR/K$DOMAIN.*.key | cut -d ':' -f1) .key) && \
-	[[ $DOMAIN == "9-rg.com" ]] && DOMAIN="9rgcom"
+	[[ $DOMAIN == "9-rg.com" ]] && DOMAIN="9rgcom" && \
 	ldns-signzone -n -p -s $(head -n 1000 /dev/random | sha1 | cut -b 1-16) -f $ZONEDIR/$DOMAIN.zone.signed $DOMAIN.zone $DNSSECDIR/$ZSK $DNSSECDIR/$KSK && \
-	[[ $DOMAIN == "9rgcom" ]] && DOMAIN="9-rg.com"
+	[[ $DOMAIN == "9rgcom" ]] && DOMAIN="9-rg.com" && \
 	nsd-control reload $DOMAIN && \
 	nsd-control notify $DOMAIN && \
 	exit
@@ -44,7 +40,6 @@ export KSK=`ldns-keygen -k -a RSASHA1-NSEC3-SHA1 -b 2048 $DOMAIN`
 mv $ZSK.* $DNSSECDIR && mv $KSK.* $DNSSECDIR
 chown root:_nsd $DNSSECDIR/* && chmod ug+r,o-rwx $DNSSECDIR/* 
 
-# now it's time to create the .signed zone file
 [[ $DOMAIN == "9-rg.com" ]] && DOMAIN="9rgcom"
 
 # now it's time to create the .signed zone file
