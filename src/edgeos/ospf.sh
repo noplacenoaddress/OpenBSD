a79f5726d8a84193ace56aac2f2a736696210211
Riccardo Giuntoli <riccardo@telecomlobby.com>
2021-08-28 11:44:03 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch taglio-28082021
     Your branch is up to date with 'origin/taglio-28082021'.
    
     Changes to be committed:
    	modified:   setup_node
    	modified:   src/edgeos/ipsec/ES--SRCID--updown.sh
    	modified:   src/edgeos/ospf.sh
    	modified:   src/edgeos/scripts/ES-SRCID-_netwatch.sh

--- src/edgeos/ospf.sh
+++ src/edgeos/ospf.sh
@@ -3,7 +3,6 @@
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ ip ospf authentication md5 key-id 1 md5-key /OSPFMD5/
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ ip ospf dead-interval 40
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ ip ospf hello-interval 10
-/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ ip ospf network point-to-point
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ ip ospf priority 1
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ ip ospf retransmit-interval 5
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces tunnel /TUN/ ip ospf transmit-delay 1
