81ae7031e07da4212ce246399175620c31f2180b
Riccardo Giuntoli <taglio@gmail.com>
2021-11-03 10:30:33 +0100

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch taglio-03112021
     Your branch is up to date with 'origin/taglio-03112021'.
    
     Changes to be committed:
    	modified:   setup_node
    	modified:   src/openbsd/pdns-schema.sql
    	new file:   src/openbsd/pdnssec-schema.sql

--- src/openbsd/pdns-schema.sql
+++ src/openbsd/pdns-schema.sql
@@ -1,3 +1,46 @@
+PRAGMA foreign_keys = 1;
+
+CREATE TABLE domains (
+  id                    INTEGER PRIMARY KEY,
+  name                  VARCHAR(255) NOT NULL COLLATE NOCASE,
+  master                VARCHAR(128) DEFAULT NULL,
+  last_check            INTEGER DEFAULT NULL,
+  type                  VARCHAR(6) NOT NULL,
+  notified_serial       INTEGER DEFAULT NULL,
+  account               VARCHAR(40) DEFAULT NULL
+);
+
+CREATE UNIQUE INDEX name_index ON domains(name);
+
+
+CREATE TABLE records (
+  id                    INTEGER PRIMARY KEY,
+  domain_id             INTEGER DEFAULT NULL,
+  name                  VARCHAR(255) DEFAULT NULL,
+  type                  VARCHAR(10) DEFAULT NULL,
+  content               VARCHAR(65535) DEFAULT NULL,
+  ttl                   INTEGER DEFAULT NULL,
+  prio                  INTEGER DEFAULT NULL,
+  disabled              BOOLEAN DEFAULT 0,
+  ordername             VARCHAR(255),
+  auth                  BOOL DEFAULT 1,
+  FOREIGN KEY(domain_id) REFERENCES domains(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE INDEX records_lookup_idx ON records(name, type);
+CREATE INDEX records_lookup_id_idx ON records(domain_id, name, type);
+CREATE INDEX records_order_idx ON records(domain_id, ordername);
+
+
+CREATE TABLE supermasters (
+  ip                    VARCHAR(64) NOT NULL,
+  nameserver            VARCHAR(255) NOT NULL COLLATE NOCASE,
+  account               VARCHAR(40) NOT NULL
+);
+
+CREATE UNIQUE INDEX ip_nameserver_pk ON supermasters(ip, nameserver);
+
+
 CREATE TABLE comments (
   id                    INTEGER PRIMARY KEY,
   domain_id             INTEGER NOT NULL,
@@ -5,97 +48,41 @@ CREATE TABLE comments (
   type                  VARCHAR(10) NOT NULL,
   modified_at           INT NOT NULL,
   account               VARCHAR(40) DEFAULT NULL,
-  comment               VARCHAR(65535) NOT NULL
+  comment               VARCHAR(65535) NOT NULL,
+  FOREIGN KEY(domain_id) REFERENCES domains(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE INDEX comments_domain_id_index ON comments (domain_id);
-CREATE INDEX comments_nametype_index ON comments (name, type);
+CREATE INDEX comments_idx ON comments(domain_id, name, type);
 CREATE INDEX comments_order_idx ON comments (domain_id, modified_at);
 
 
-BEGIN TRANSACTION;
-  CREATE TEMPORARY TABLE records_backup(
-    id                  INTEGER PRIMARY KEY,
-    domain_id           INTEGER DEFAULT NULL,
-    name                VARCHAR(255) DEFAULT NULL,
-    type                VARCHAR(10) DEFAULT NULL,
-    content             VARCHAR(65535) DEFAULT NULL,
-    ttl                 INTEGER DEFAULT NULL,
-    prio                INTEGER DEFAULT NULL,
-    change_date         INTEGER DEFAULT NULL,
-    ordername           VARCHAR(255),
-    auth                BOOL DEFAULT 1
-  );
+CREATE TABLE domainmetadata (
+ id                     INTEGER PRIMARY KEY,
+ domain_id              INT NOT NULL,
+ kind                   VARCHAR(32) COLLATE NOCASE,
+ content                TEXT,
+ FOREIGN KEY(domain_id) REFERENCES domains(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
-  INSERT INTO records_backup SELECT id,domain_id,name,type,content,ttl,prio,change_date,ordername,auth FROM records;
-  DROP TABLE records;
+CREATE INDEX domainmetaidindex ON domainmetadata(domain_id);
+CREATE TABLE cryptokeys (
+ id                     INTEGER PRIMARY KEY,
+ domain_id              INT NOT NULL,
+ flags                  INT NOT NULL,
+ active                 BOOL,
+ published              BOOL DEFAULT 1,
+ content                TEXT,
+ FOREIGN KEY(domain_id) REFERENCES domains(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
-  CREATE TABLE records (
-    id                  INTEGER PRIMARY KEY,
-    domain_id           INTEGER DEFAULT NULL,
-    name                VARCHAR(255) DEFAULT NULL,
-    type                VARCHAR(10) DEFAULT NULL,
-    content             VARCHAR(65535) DEFAULT NULL,
-    ttl                 INTEGER DEFAULT NULL,
-    prio                INTEGER DEFAULT NULL,
-    change_date         INTEGER DEFAULT NULL,
-    disabled            BOOLEAN DEFAULT 0,
-    ordername           VARCHAR(255),
-    auth                BOOL DEFAULT 1
-  );
-
-  CREATE INDEX rec_name_index ON records(name);
-  CREATE INDEX nametype_index ON records(name,type);
-  CREATE INDEX domain_id ON records(domain_id);
-  CREATE INDEX orderindex ON records(ordername);
-
-  INSERT INTO records SELECT id,domain_id,name,type,content,ttl,prio,change_date,0,ordername,auth FROM records_backup;
-  DROP TABLE records_backup;
-COMMIT;
+CREATE INDEX domainidindex ON cryptokeys(domain_id);
 
 
-BEGIN TRANSACTION;
-  CREATE TEMPORARY TABLE supermasters_backup (
-    ip                  VARCHAR(64) NOT NULL,
-    nameserver          VARCHAR(255) NOT NULL COLLATE NOCASE,
-    account             VARCHAR(40) DEFAULT NULL
-  );
+CREATE TABLE tsigkeys (
+ id                     INTEGER PRIMARY KEY,
+ name                   VARCHAR(255) COLLATE NOCASE,
+ algorithm              VARCHAR(50) COLLATE NOCASE,
+ secret                 VARCHAR(255)
+);
 
-  INSERT INTO supermasters_backup SELECT ip,nameserver,account FROM supermasters;
-  UPDATE supermasters_backup SET account='' WHERE account IS NULL;
-  DROP TABLE supermasters;
-
-CREATE TABLE supermasters (
-     ip                  VARCHAR(64) NOT NULL,
-     nameserver          VARCHAR(255) NOT NULL COLLATE NOCASE,
-     account             VARCHAR(40) NOT NULL
-   );
-   CREATE UNIQUE INDEX ip_nameserver_pk ON supermasters(ip, nameserver);
-
-   INSERT INTO supermasters SELECT ip,nameserver,account FROM supermasters_backup;
-   DROP TABLE supermasters_backup;
- COMMIT;
-
-
- BEGIN TRANSACTION;
-   CREATE TABLE domainmetadata_backup (
-     id INTEGER PRIMARY KEY,
-     domain_id INT NOT NULL,
-     kind VARCHAR(32) COLLATE NOCASE,
-     content TEXT
-   );
-
-   INSERT INTO domainmetadata_backup SELECT id,domain_id,kind,content FROM domainmetadata;
-   DROP TABLE domainmetadata;
-
-   CREATE TABLE domainmetadata (
-     id INTEGER PRIMARY KEY,
-     domain_id INT NOT NULL,
-     kind VARCHAR(32) COLLATE NOCASE,
-     content TEXT
-   );
-   CREATE INDEX domainmetaidindex ON domainmetadata(domain_id);
-
-   INSERT INTO domainmetadata SELECT id,domain_id,kind,content FROM domainmetadata_backup;
-   DROP TABLE domainmetadata_backup;
- COMMIT;
+CREATE UNIQUE INDEX namealgoindex ON tsigkeys(name, algorithm);
