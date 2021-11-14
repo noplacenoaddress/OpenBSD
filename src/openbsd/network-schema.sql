78dead25ffa91f26a9c790ff2cf30ecef00f031d
Riccardo Giuntoli <taglio@gmail.com>
2021-11-14 20:33:06 +0100

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch taglio-14112021
     Your branch is up to date with 'origin/taglio-14112021'.
    
     Changes to be committed:
    	modified:   console
    	modified:   setup_node
    	modified:   src/openbsd/network-schema.sql

--- src/openbsd/network-schema.sql
+++ src/openbsd/network-schema.sql
@@ -1,13 +1,12 @@
 PRAGMA foreign_keys = 1;
 
 CREATE TABLE domains (
-  id                    INTEGER PRIMARY KEY,
+  id                    INTEGER PRIMARY KEY AUTOINCREMENT,
   name                  VARCHAR(255) NOT NULL COLLATE NOCASE,
-  ns1                   VARCHAR(40) DEFAULT NULL,
-  ns2                   VARCHAR(40) DEFAULT NULL,
-  ns3                   VARCHAR(40) DEFAULT NULL,
+  ns34                  VARCHAR(40) DEFAULT NULL,
+  ns12                  VARCHAR(40) DEFAULT NULL,
+  ns56                  VARCHAR(40) DEFAULT NULL,
   dnssec                TINYINT DEFAULT 0,
   dnssec_keyid          VARCHAR(5) DEFAULT NULL,
   last_update           INTEGER DEFAULT NULL,
-  last_check            INTEGER DEFAULT NULL,
 );
