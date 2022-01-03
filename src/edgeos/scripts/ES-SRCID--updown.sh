a2e432509d233487b6dfcf76220e38bf8803beb4
Riccardo Giuntoli <taglio@gmail.com>
2022-01-03 10:18:08 +0100

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch taglio-03012022
     Your branch is up to date with 'origin/taglio-03012022'.
    
     Changes to be committed:
    	modified:   setup_node
    	modified:   src/edgeos/scripts/ES-SRCID--updown.sh

--- src/edgeos/scripts/ES-SRCID--updown.sh
+++ src/edgeos/scripts/ES-SRCID--updown.sh
@@ -4,18 +4,21 @@ set -o nounset
 set -o errexit
 
 TUN_IFACE="/TUN/"
+PREFIX="/ROUTERIDLAST/"
 
 case "${PLUTO_VERB}" in
-	up-host)
-		echo "Putting interface ${TUN_IFACE} up"
-		ifconfig $TUN_IFACE up
-		echo "Disabling IPsec policy (SPD) for ${TUN_IFACE}"
-		sysctl -w "net.ipv4.conf.${TUN_IFACE}.disable_policy=1"
-		echo "Accepting gre keepalive"
-		sysctl -w "net.ipv4.conf.${TUN_IFACE}.accept_local=1"
+	up-host|up-client)
+		/usr/bin/logger "Putting interface ${TUN_IFACE} up"
+		/sbin/ip link set dev "${TUN_IFACE}" up
+		/usr/bin/logger "Disabling IPsec policy (SPD) for ${TUN_IFACE}"
+		/sbin/sysctl -w "net.ipv4.conf.${TUN_IFACE}.disable_policy=1"
+		/usr/bin/logger "Accepting gre keepalive"
+		/sbin/sysctl -w "net.ipv4.conf.${TUN_IFACE}.accept_local=1"
+		/sbin/ip route add table $PREFIX scope link default nexthop dev "${TUN_IFACE}"
+		#OTHERS
 		;;
-	down-host)
-		ifconfig $TUN_IFACE down
+	down-host|down-client)
+		logger "${TUN_IFACE} down"
+		/sbin/ip link set dev "${TUN_IFACE}" down
 		;;
 esac
-
