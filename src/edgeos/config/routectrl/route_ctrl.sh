94d1a1e759abfd7f75ea09b0f1049ae8dff5e6de
Riccardo Giuntoli <taglio@gmail.com>
2022-03-15 23:53:41 +0100

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch taglio-15032022
     Your branch is up to date with 'origin/taglio-15032022'.
    
     Changes to be committed:
    	modified:   src/edgeos/config/routectrl/route_ctrl.sh

--- src/edgeos/config/routectrl/route_ctrl.sh
+++ src/edgeos/config/routectrl/route_ctrl.sh
@@ -3,6 +3,30 @@
 
 declare -a groups=("12" "34" "43" "56")
 
+# initialize a semaphore with a given number of tokens
+open_sem(){
+    mkfifo pipe-$$
+    exec 3<>pipe-$$
+    rm pipe-$$
+    local i=$1
+    for((;i>0;i--)); do
+        printf %s 000 >&3
+    done
+}
+
+# run the given command asynchronously and pop/push tokens
+run_with_lock(){
+    local x
+    # this read waits until there is something to read
+    read -u 3 -n 3 x && ((0==x)) || exit $x
+    (
+     ( "$@"; )
+    # push the return code of the command to the semaphore
+    printf '%.3d' $? >&3
+    )&
+}
+
+
 if [[ $# -eq 0 ]]; then
 	echo -e $0 "have to be used with the following options \
 			\n \
