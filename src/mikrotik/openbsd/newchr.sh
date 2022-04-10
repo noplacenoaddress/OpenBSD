b870fadebac8dfc8db5b039fd2c8312161b2a784
Riccardo Giuntoli <taglio@gmail.com>
2022-04-10 18:41:21 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch taglio-10042022
     Your branch is up to date with 'origin/taglio-10042022'.
    
     Changes to be committed:
    	modified:   src/mikrotik/openbsd/newchr.sh

--- src/mikrotik/openbsd/newchr.sh
+++ src/mikrotik/openbsd/newchr.sh
@@ -9,8 +9,8 @@ cd "${tmpdir}"
 tar -xvf ../"${file}"
 mv tmp/*/* .
 rm -rf tmp/
-phn=$(cat hostname.enc? | head -n 1 | awk '{print $2}' | sed "s|\"||g")
-x=$(cat hostname.gre? | grep gre | head -n 1 | awk '{print $2}' | sed "s|gre||")
+phn=$(cat hostname.enc*[0-9] | head -n 1 | awk '{print $2}' | sed "s|\"||g")
+x=$(cat hostname.gre*[0-9] | grep gre | head -n 1 | awk '{print $2}' | sed "s|gre||")
 install -o root -g wheel -m 0640 hostname.enc? /etc
 install -o root -g wheel -m 0640 hostname.gre? /etc
 install -o root -g wheel -m 0640 iked.conf "/etc/iked.conf.${phn}"
