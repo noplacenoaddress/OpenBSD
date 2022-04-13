e2d9e0293c5fd0cc4e82b9d6ee0fc5c903d2b7c8
Riccardo Giuntoli <taglio@gmail.com>
2022-04-13 10:58:06 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch taglio-13042022
     Changes to be committed:
    	modified:   src/etc/ifstated.conf
    	modified:   src/root/Bin/ifstated_dns.sh
    	modified:   src/root/Bin/lteddns.sh

--- src/root/Bin/lteddns.sh
+++ src/root/Bin/lteddns.sh
@@ -4,5 +4,13 @@ let counter=1
 while ((counter<5)); do
   ((counter=counter+1))
   /bin/sleep $((10-$(/bin/date +%s)%10))
-  /usr/sbin/unbound-control flush_zone mynetname.net ; /sbin/pfctl -t lte -T kill ; /sbin/pfctl -t lte -T add -f /etc/pf.conf.table.lte
+  /usr/sbin/unbound-control flush_zone mynetname.net
+  for ddns in $(cat /etc/pf.conf.table.lte | grep mynetname.net); do
+      oldip=$(dig A "${ddns}" +short)
+      (( $(/sbin/pfctl -t lte -T show | grep -c "${oldip}") == 0 )) && (
+        /usr/sbin/ikectl reload
+        /sbin/pfctl -t lte -T kill
+        /sbin/pfctl -t lte -T add -f /etc/pf.conf.table.lte
+      )
+  done
 done
