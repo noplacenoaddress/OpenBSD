c0f05002e0dc31528294859eec3ccae451ce2ef0
Riccardo Giuntoli <taglio@gmail.com>
2022-04-18 17:02:23 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch taglio-18042022
     Your branch is up to date with 'origin/taglio-18042022'.
    
     Changes to be committed:
    	modified:   src/root/Bin/ipsec_certs.sh

--- src/root/Bin/ipsec_certs.sh
+++ src/root/Bin/ipsec_certs.sh
@@ -3,3 +3,4 @@
 for f in $(ls -al /home/taglio/Sources/Git/OpenBSD/src/etc/iked/certs/*.crt | awk '{print $9}');  do install -o root -g wheel -m 0444 "${f}" /etc/iked/certs/; done
 for f in $(ls -al /home/taglio/Sources/Git/OpenBSD/src/etc/iked/pubkeys/fqdn/*telecomlobby* | awk '{print $9}');  do install -o root -g wheel -m 0444 "${f}" /etc/iked/pubkeys/fqdn/; done
 for f in $(ls -al /home/taglio/Sources/Git/OpenBSD/src/etc/iked/pubkeys/ufqdn/*telecomlobby* | awk '{print $9}');  do install -o root -g wheel -m 0444 "${f}" /etc/iked/pubkeys/ufqdn/; done
+/usr/sbin/ikectl reload
