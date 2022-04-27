ae271ca87e55ffce0ea9cb37c5425c06c5af14b3
Riccardo Giuntoli <taglio@gmail.com>
2022-04-27 09:15:39 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch taglio-27042022
     Your branch is up to date with 'origin/taglio-27042022'.
    
     Changes to be committed:
    	new file:   src/openbsd/dotfiles-master/.Xdefaults
    	new file:   src/openbsd/dotfiles-master/.config/images/lockicon.png
    	new file:   src/openbsd/dotfiles-master/.config/images/wallpaper.png
    	new file:   src/openbsd/dotfiles-master/.config/lock.sh
    	new file:   src/openbsd/dotfiles-master/.config/rofi/config
    	new file:   src/openbsd/dotfiles-master/.conkyrc
    	new file:   src/openbsd/dotfiles-master/.fvwmrc
    	new file:   src/openbsd/dotfiles-master/.vimrc
    	new file:   src/openbsd/dotfiles-master/README.md
    	new file:   src/openbsd/dotfiles-master/install.sh
    	new file:   src/openbsd/dotfiles-master/openbsd-dark/background.png
    	new file:   src/openbsd/dotfiles-master/openbsd-dark/panel.png
    	new file:   src/openbsd/dotfiles-master/openbsd-dark/slim.theme
    	new file:   src/openbsd/dotfiles-master/screenshots.png
    	new file:   src/openbsd/dotfiles.tar.gz

---
+++ src/openbsd/dotfiles-master/install.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+echo -n "Install additional software? [y/n] "
+read install
+
+echo -n "Move dotfiles to which user? "
+read user
+
+if [[ $install == y ]]; then
+    pkg_add wireshark firefox gimp vlc transmission-gtk git vim nnn cmus
+fi
+
+echo "[*] Installing necessary software..."
+pkg_add feh conky i3lock scrot rofi ImageMagick slim slim-themes
+
+echo "[*] Copying dotfiles..."
+cp .fvwmrc /home/$user/.fvwmrc
+cp .conkyrc /home/$user/.conkyrc
+cp .vimrc /home/$user/.vimrc
+cp .Xdefaults /home/$user/.Xdefaults
+mv .config /home/$user/
+
+echo "conky &" > /home/$user/.xinitrc
+echo "exec fvwm" >> /home/$user/.xinitrc
+
+chown -R $user /home/$user
+
+cp -R ./openbsd-dark /usr/local/share/slim/themes/
+cat <<EOT >> /etc/rc.local
+if [ -x /usr/local/bin/slim ]; then
+    echo -n ' slim'; ( sleep 5; /usr/local/bin/slim -nodaemon ) &
+fi
+EOT
+echo "[*] Change the variable current_theme in /etc/slim.conf to openbsd-dark"
+
+echo "[*] Done."
